f721a9e93a32be00715ed348bf0bbf890b3ae873
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2022-06-04 21:45:02 +0530

    first commit

---
+++ lib/features/auth/screens/auth_screen.dart
@@ -0,0 +1,63 @@
+import 'package:amazon_full_stack/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+enum Auth { signin, signup }
+
+class AuthScreen extends StatefulWidget {
+  static const String routeName = '/auth-screen';
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signup;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Text(
+              'Welcome',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            ListTile(
+              title: const Text(
+                'Create Account',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signup,
+                  groupValue: _auth,
+                  onChanged: (Auth? val) {
+                    setState(() {
+                      _auth = val!;
+                    });
+                  }),
+            ),
+            ListTile(
+              title: const Text(
+                'Create Account',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signin,
+                  groupValue: _auth,
+                  onChanged: (Auth? val) {
+                    setState(() {
+                      _auth = val!;
+                    });
+                  }),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:amazon_full_stack/constants/global_variables.dart';
+import 'package:amazon_full_stack/features/auth/screens/auth_screen.dart';
+import 'package:amazon_full_stack/router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Amazon Clone',
+        theme: ThemeData(
+            scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+            colorScheme: const ColorScheme.light(
+                primary: GlobalVariables.secondaryColor),
+            appBarTheme: const AppBarTheme(
+                elevation: 0, iconTheme: IconThemeData(color: Colors.black))),
+        onGenerateRoute: (settings) => generateRoute(settings),
+        home: const AuthScreen());
+    // Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text('Amazon'),
+    //   ),
+    //   body: Column(
+    //     children: [
+    //       const Center(child: Text('Flutter Demo Home Page')),
+    //       Builder(builder: (context) {
+    //         return ElevatedButton(
+    //             onPressed: () {
+    //               Navigator.pushNamed(context, AuthScreen.routeName);
+    //             },
+    //             child: const Text('Click'));
+    //       })
+    //     ],
+    //   ),
+    // ));
+  }
+}
