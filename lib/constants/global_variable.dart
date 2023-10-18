ee4f96c993262adedac95f4e4ee47262d7dcd6f1
Nishchay Shakya <79781577+NishchayShakya1@users.noreply.github.com>
2023-10-18 12:48:26 +0530

    uri

--- lib/constants/global_variable.dart
+++ lib/constants/global_variable.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://172.22.21.22:3000';
+String uri = 'http://https://timely-souffle-6a5303.netlify.app:3000';
 
 class GlobalVariables {
   // COLORS
